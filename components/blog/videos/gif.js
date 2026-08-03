c0af6b8390a4d42cadd89e7aedaf337482558794
Nash Bostwick <nashb1323@gmail.com>
2026-08-03 00:42:25 -0800

    added wedge article

--- components/blog/videos/gif.js
+++ components/blog/videos/gif.js
@@ -1,29 +1,37 @@
-//TODO: Remove this component. GIF's can be displied with any of the image components.
+'use client'
 
-
+import { useState } from 'react'
 
 export function Gif({video, topTitle, bottomTitle, height, width}){
+
+    const [ready, setReady] = useState(false)
 
     return(
         <>
             { topTitle && <p className='m-0'>{topTitle}</p> }
             <div className='flex flex-col justify-center items-center w-full'>
-                <video 
-                    width={width} 
-                    height={height}
-                    preload="none"
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                    className='m-0 bg-blue-700'
+                <div
+                    className='relative m-0 max-w-full'
+                    style={{ width, aspectRatio: width && height ? `${width} / ${height}` : undefined }}
                 >
-                    <source src={video} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
+                    { !ready && <div className='absolute inset-0 bg-gray-200 animate-pulse' /> }
+                    <video
+                        width={width}
+                        height={height}
+                        preload="none"
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                        onPlaying={() => setReady(true)}
+                        className={`m-0 w-full h-full transition-opacity duration-300 ${ready ? 'opacity-100' : 'opacity-0'}`}
+                    >
+                        <source src={video} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
             </div>
             { bottomTitle && <p className='m-0'>{bottomTitle}</p> }
         </>
     )
 }
-
