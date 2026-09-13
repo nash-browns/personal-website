2d1cfa4b9c6a6e8bed28c4e4567c8eef6d2b1a4e
Nash Bostwick <nashb1323@gmail.com>
2026-09-12 22:32:11 -0400

    updated product links

--- products/product-links.js
+++ products/product-links.js
@@ -1,40 +1,17 @@
-//Consumables
-export const PlumbersPutty = ({title='Plumbers Putty'} = {}) => <a href='https://amzn.to/3VvHUIa' target="_blank">{title}</a>
-
 //TOOLS
-export const DrillBits = ({title='Drill Bits'} = {}) => <a href='https://amzn.to/3yMPtlP' target="_blank">{title}</a>
-export const Drill = ({title='Drill'} = {}) => <a href='https://amzn.to/4bKPfuh' target="_blank">{title}</a>
-export const BoltCutters = ({title='Bolt Cutters'} = {}) => <a href='https://amzn.to/3RhwVAK' target="_blank">{title}</a>
-export const AngleGrinder = ({title='Angle Grinder'} = {}) => <a href='https://amzn.to/3KvSMR1' target="_blank">{title}</a>
-export const HotKnife = ({title='Hot Knife'} = {}) => <a href='https://amzn.to/3P0gBCL' target="_blank">{title}</a>
-export const DryWallTSquare = ({title='Drywall T-Square'} = {}) => <a href='https://amzn.to/4flrY2T' target="_blank">{title}</a>
-export const SpeedSquare = ({title='Speed Square'} = {}) => <a href='https://amzn.to/3BHFNuD' target="_blank">{title}</a>
-export const ElectricMultiTool = ({title='Electric Multi-Tool'} = {}) => <a href='https://amzn.to/3ZWlKlr' target="_blank">{title}</a>
-export const Scissors = ({title='Scissors'} = {}) => <a href='https://amzn.to/4iE9c9U' target="_blank">{title}</a>
-
-//ACTIONPACKER
-export const ActionPacker8 = ({title='ActionPacker'} = {}) => <a href='https://amzn.to/3VxhExW' target="_blank">{title}</a>
-export const ActionPacker24 = ({title='ActionPacker'} = {}) => <a href='https://amzn.to/3yWOTSt' target="_blank">{title}</a>
-export const ActionPacker35 = ({title='ActionPacker'} = {}) => <a href='https://amzn.to/4aTfblW' target="_blank">{title}</a>
-export const ActionPacker35Wheeled = ({title='ActionPacker'} = {}) => <a href='https://amzn.to/3x6U1TA' target="_blank">{title}</a>
-export const ActionPacker48 = ({title='ActionPacker'} = {}) => <a href='https://amzn.to/3Ky8Pha' target="_blank">{title}</a>
-export const RatchetStraps = ({title='Ratchet Strap'} = {}) => <a href='https://amzn.to/3Rdnn9K' target="_blank">{title}</a>
-export const Handles = ({title='Handles'} = {}) => <a href='https://amzn.to/3xdjvyE' target="_blank">{title}</a>
-
-//MAG-LIGHT
-export const ButtonRubber = ({title='Button Rubber'} = {}) => <a href='https://amzn.to/3wWol3r' target="_blank">{title}</a>
-
-//Ryobi Link
-export const RyobiLinkBoxMedium = ({title='Ryobi Link Medium Box'} = {}) => <a href='https://amzn.to/3VqOJKO' target="_blank">{title}</a>
+export const HotKnife = ({title='Hot Knife'} = {}) => <a href='https://link.amazon/B03VIeR4p' target="_blank">{title}</a>
+export const DryWallTSquare = ({title='Drywall T-Square'} = {}) => <a href='https://link.amazon/B01hJKnMh' target="_blank">{title}</a>
+export const SpeedSquare = ({title='Speed Square'} = {}) => <a href='https://link.amazon/B0iaDwRiI' target="_blank">{title}</a>
+export const ElectricMultiTool = ({title='Electric Multi-Tool'} = {}) => <a href='https://link.amazon/B0cpKFtCN' target="_blank">{title}</a>
+export const Scissors = ({title='Scissors'} = {}) => <a href='https://link.amazon/B0cEFXecC' target="_blank">{title}</a>
 
 //Tune M1
-export const FriggaTempLoger = ({title='Frigga Temperature Logger'} = {}) => <a href='https://amzn.to/3ZRcoaG' target="_blank">{title}</a>
-export const ReflectiveInsulation = ({title='Reflective Foil Insulation'} = {}) => <a href='https://amzn.to/3BrUbHu' target="_blank">{title}</a>
-export const OutdoorCarpet = ({title='Outdoor Carpet'} = {}) => <a href='https://amzn.to/3VE1ov1' target="_blank">{title}</a>
-export const HiStrengthAdhesive = ({title='Hi Strength Spray Adhesive'} = {}) => <a href='https://amzn.to/3VByfQY' target="_blank">{title}</a>
-export const VelcroRoll = ({title='Velcro Roll'} = {}) => <a href='https://amzn.to/4iDggDB' target="_blank">{title}</a>
-export const FriggaLogger = ({title='Frigga Temperature Logger'} = {}) => <a href='https://amzn.to/3PCN6r8' target="_blank">{title}</a>
+export const FriggaLogger = ({title='Frigga Temperature Logger'} = {}) => <a href='https://link.amazon/B0fjLHEdQ' target="_blank">{title}</a>
+export const ReflectiveInsulation = ({title='Reflective Foil Insulation'} = {}) => <a href='https://link.amazon/B0in7edjX' target="_blank">{title}</a>
+export const OutdoorCarpet = ({title='Outdoor Carpet'} = {}) => <a href='https://link.amazon/B0bpbN1OK' target="_blank">{title}</a>
+export const HiStrengthAdhesive = ({title='Hi Strength Spray Adhesive'} = {}) => <a href='https://link.amazon/B02UgJtqK' target="_blank">{title}</a>
+export const VelcroRoll = ({title='Velcro Roll'} = {}) => <a href='https://link.amazon/B00gy48JT' target="_blank">{title}</a>
 export const StarlinkMount = ({title='TRIO Speedmount for Starlink Mini'} = {}) => <a href='https://mobilemusthave.com/products/trio-speedmount-for-starlink-mini' target="_blank">{title}</a>
-export const TrailerConnector = ({title='4 Way Flat Trailer Connector'} = {}) => <a href={`https://www.amazon.com/Trailer-Harness-Replacement-Connector-Multi-Purpose/dp/B0DCNDDPJR/ref=asc_df_B0DCNDDPJR?mcid=16468ca4f7ad3751857b410dcdd7cf3f&hvocijid=10910222517954454704-B0DCNDDPJR-&hvexpln=73&tag=hyprod-20&linkCode=df0&hvadid=721245378154&hvpos=&hvnetw=g&hvrand=10910222517954454704&hvpone=`} target="_blank">{title}</a>
+export const TrailerConnector = ({title='4 Way Flat Trailer Connector'} = {}) => <a href={`https://link.amazon/B0gbNxR9J`} target="_blank">{title}</a>
 export const TacomaConnectors = ({title='Tacoma Brake Light Connectors'} = {}) => <a href={`https://www.ebay.com/itm/365617934332`} target="_blank">{title}</a>
-export const GenaricConnector = ({title='Generic Connectors'} = {}) => <a href={`https://www.amazon.com/dp/B0CQSC5GYK?ref=ppx_yo2ov_dt_b_fed_asin_title`} target="_blank">{title}</a>
+export const GenaricConnector = ({title='Generic Connectors'} = {}) => <a href={`https://link.amazon/B05kDKFam`} target="_blank">{title}</a>
